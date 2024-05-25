06dfd21f94438cae822804171a406e6242da426e
slaqy <cthulhu@slaqy.com>
2024-05-25 19:01:30 +0200

    Under construction commit.

---
+++ src/assets/data.js
@@ -0,0 +1,31 @@
+
+
+export const socials = [
+    {
+        logo: 'bx bxl-github' 
+    },
+    {
+        logo: 'bx bxl-linkedin-square' 
+    },
+    {
+        logo: 'bx bxl-github' 
+    },
+    {
+        logo: 'bx bxl-linkedin-square' 
+    },
+    
+]
+
+
+export const info = [
+    {
+        text: 'About'
+    },
+    {
+        text: 'Projects'
+    },
+    {
+        text: 'About'
+    }
+
+]
